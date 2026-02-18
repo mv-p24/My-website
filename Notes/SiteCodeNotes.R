f1916f4dc40fbcce677d3ff857c22eeb5c96f880
mv-p24 <mvarkony@gmail.com>
2026-02-17 21:23:47 -0500

    Add custom images, notes, and blog draft

---
+++ Notes/SiteCodeNotes.R
@@ -0,0 +1,14 @@
+#To Preview Site
+
+blogdown::serve_site()
+
+
+# Any time Change config.toml use this command to check 
+blogdown::check_config()
+
+#Creates/Opens Netlify Build of Site
+blogdown::config_netlify()
+
+#Check 
+blogdown::check_netlify()
+
